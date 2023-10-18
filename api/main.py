d4bcec3cc6ab26ef94579ab0dc662913e3824e68
Arie Kurniawan <hubungi.aja@gmail.com>
2023-10-18 18:54:24 +0700

    Remove landing pages

--- api/main.py
+++ api/main.py
@@ -2,11 +2,10 @@
 
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from jinja2.exceptions import TemplateNotFound
 from starlette.middleware.sessions import SessionMiddleware
 from .routes.auth import session as session_router
 from .routes.dashboard import dashboard as dashboard_router
@@ -34,20 +33,3 @@ app.add_middleware(
 
 # Mount the static files with absolute paths
 app.mount("/assets", StaticFiles(directory=os.path.join(project_dir, 'frontend', 'assets')), name="assets")
-
-# Mount the favicon.ico
-# app.mount("/favicon.ico", StaticFiles(directory=os.path.join(project_dir, 'frontend', 'theme-assets')), name="favicon")
-
-# Set up templates with absolute paths
-landingPages = Jinja2Templates(directory=os.path.join(project_dir, 'frontend', 'landing-pages'))
-
-@app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return landingPages.TemplateResponse("/index.html", {"request": request})
-
-@app.get("/{filename}", response_class=HTMLResponse)
-async def read_file(filename: str, request: Request):
-    try:
-        return landingPages.TemplateResponse(f"/{filename}.html", {"request": request})
-    except TemplateNotFound:
-        raise HTTPException(status_code=404, detail="File not found")
--- api/routes/dashboard.py
+++ api/routes/dashboard.py
@@ -15,9 +15,9 @@ dashboardPages = Jinja2Templates(directory=os.path.join(project_dir, 'frontend',
 async def read_root(request: Request):
     return dashboardPages.TemplateResponse("documentations.html", {"request": request})
 
-@dashboard.get("/dashboard", response_class=HTMLResponse)
+@dashboard.get("/", response_class=HTMLResponse)
 async def read_root(request: Request, current_user: dict = Depends(get_current_user)):
     if current_user is None:
         return RedirectResponse(url="/auth/signin", status_code=status.HTTP_303_SEE_OTHER)
 
-    return dashboardPages.TemplateResponse("index-boxed-fancy.html", {"request": request})
+    return dashboardPages.TemplateResponse("index.html", {"request": request})
